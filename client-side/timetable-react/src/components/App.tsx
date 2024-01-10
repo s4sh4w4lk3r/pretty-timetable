498b1a37bf77f5527f1c5b0ebd5335c06a67df35
Aleksandr <sanya.romaniuta@gmail.com>
2024-01-10 21:33:28 +0300

    перенес квери расписания в компонент таблицы, добавил стейт для выбора подгруппы

--- client-side/timetable-react/src/components/App.tsx
+++ client-side/timetable-react/src/components/App.tsx
@@ -1,48 +1,30 @@
 import { useState } from "react"
 import Table from "./Table/Table";
 import styles from "./App.module.css"
-import { useQuery } from "@apollo/client";
-import { ACTUAL_TIMETABLE_BY_GROUP_ID_WEEKNUMBER } from "../api/graphql/queries";
-import { ActualTimetable } from "../api/graphql/__generated__/graphql";
-
+import { SubGroup } from "../api/graphql/__generated__/graphql";
 
 function getWeekNumber(date: Date) {
     date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
     date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
     var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
         - 3 + (week1.getDay() + 6) % 7) / 7);
 }
 
 
 export default function App() {
-    const testDate = new Date(2024, 8, 2);
+    const date = new Date(2024, 8, 2);
 
-    const currentWeekNumber = getWeekNumber(testDate);
-    const [groupId, setGroupId] = useState<number>(0);
-    // сделать так чтобы фетча при нуле не было
-    const { loading, error, data } = useQuery(ACTUAL_TIMETABLE_BY_GROUP_ID_WEEKNUMBER, { variables: { weekNumber: currentWeekNumber, groupId: groupId } });
-    const timetable = data?.actualTimetables[0] as ActualTimetable;
-    if (data?.actualTimetables[0] === undefined || data?.actualTimetables[0].cards?.length === 0) {
-        return <>
-        Ошибочка
-        <input type="text" id="da" onChange={(c) => setGroupId(Number.parseInt(c.target.value))} />
-        </>
-    }
+    const currentWeekNumber = getWeekNumber(date);
+    const [groupId, setGroupId] = useState<number>(1);
+    const [subGroup, setSubGroup] = useState<SubGroup>(SubGroup.All);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-    debugger;
 
     return (
         <>
             <div className={styles.appGrid}>
-                <Table timetable={timetable}></Table>
+                <Table groupId={groupId} weekNumber={currentWeekNumber} subGroup={subGroup}></Table>
             </div>
-            <input type="text" id="da" onChange={(c) => setGroupId(Number.parseInt(c.target.value))} />
         </>
     )
 
--- client-side/timetable-react/src/components/Table/Table.tsx
+++ client-side/timetable-react/src/components/Table/Table.tsx
@@ -1,17 +1,33 @@
+import { useQuery } from "@apollo/client";
 import { ActualCard, ActualTimetable } from "../../api/graphql/__generated__/graphql";
+import { ACTUAL_TIMETABLE_BY_GROUP_ID_WEEKNUMBER } from "../../api/graphql/queries";
 import CardContainer from "../CardContainer/CardContainer";
 import styles from "./Table.module.css"
 
-type Props = { timetable: ActualTimetable }
+type Props = { groupId: number, subGroup: string, weekNumber: number }
 export default function Table(props: Props) {
 
-    const cards = props.timetable.cards as ActualCard[];
+    const { loading, error, data } = useQuery(ACTUAL_TIMETABLE_BY_GROUP_ID_WEEKNUMBER, {
+        variables: {
+            weekNumber: props.weekNumber, groupId: props.groupId
+        }
+    });
+
+    if (loading) return <p>Загрзука...</p>;
+    if (error) return <p>Ошибка : {error.message}</p>;
+
+    const timetable = data?.actualTimetables[0] as ActualTimetable;
+
+    if (timetable === undefined || timetable.cards === undefined) return <p>Нет данных</p>;
+
+    const cards = timetable.cards as ActualCard[];
     const dates = [... new Set(cards.map(c => c.date))]
         .map(d => new Date(d))
         .sort((date1, date2) => date1.getTime() > date2.getTime() ? 1 : -1);
 
     const cardContainersElement = dates.map(date => {
-        const cardsProp = cards.filter(c => new Date(c.date).getTime() === date.getTime());
+        const cardsProp = cards.filter(c => new Date(c.date).getTime() === date.getTime())
+        .filter(c=> c.subGroup === "ALL" || c.subGroup === props.subGroup);
         const dayOfWeekProp = date.toLocaleString("RU-ru", { weekday: "short" }).toUpperCase();
 
         return <CardContainer key={dayOfWeekProp} cards={cardsProp} dayOfWeek={dayOfWeekProp}></CardContainer>
@@ -19,7 +35,7 @@ export default function Table(props: Props) {
 
     return (
         <div className={styles.table}>
-            <p className={styles.groupName}>{props.timetable.group.name}</p>
+            <p className={styles.groupName}>{timetable.group.name}</p>
             <div className={styles.cardContainers}>
                 {cardContainersElement}
             </div>
