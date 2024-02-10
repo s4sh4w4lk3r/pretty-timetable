718e4a2b9cdb4758609228ba00b2f871d3521502
Aleksandr <sanya.romaniuta@gmail.com>
2024-02-10 12:09:07 +0300

    добавил immer и добавил скелетон уже на страницу

---
+++ client-side/timetable-next/app/[...group]/loading.tsx
@@ -0,0 +1,6 @@
+import CardBoxesSkeleton from "@/components/timetable/CardBoxSkeleton";
+import React from "react";
+
+export default function loading() {
+    return <CardBoxesSkeleton></CardBoxesSkeleton>;
+}
