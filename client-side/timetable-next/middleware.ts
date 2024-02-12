36be08792280c4fb68a599f21d5141709191bf6a
Aleksandr <sanya.romaniuta@gmail.com>
2024-02-12 21:44:29 +0300

    добавил матчер аутентификации

--- client-side/timetable-next/middleware.ts
+++ client-side/timetable-next/middleware.ts
@@ -1,7 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    // matcher: ["/admin/:path*"],
-    matcher: ["/admin/groups", "/admin/rooms", "/admin/subjects", "/admin/teachers", "/admin/timetables"],
-    // FIXME: сделать как-нибудь, чтобы /admin пускало всех, а на эндпоинты только админов одним реджексом
+    matcher: ["/admin/:path+"],
 };
