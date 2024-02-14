386ac17bd162477a19e86260cc1c61e3f5bb05f9
Aleksandr <sanya.romaniuta@gmail.com>
2024-02-14 10:30:58 +0300

    изменил время, через которое будет подсветка некст пары

--- client-side/timetable-next/utils/cardHints.ts
+++ client-side/timetable-next/utils/cardHints.ts
@@ -4,7 +4,10 @@ export function defineHintsRequired(params: { lessonStartsAt: string; lessonEnds
     isNow: boolean;
     isPending: boolean;
 } {
+    const minutesBeforeStartLesson = 20;
+
     const { dateFromCard, lessonEndsAt, lessonStartsAt } = params;
+
     const startDate = new Date(new Date().setHours(Number.parseInt(lessonStartsAt.split(":")[0]), Number.parseInt(lessonStartsAt.split(":")[1]), 0));
     const endDate = new Date(new Date().setHours(Number.parseInt(lessonEndsAt.split(":")[0]), Number.parseInt(lessonEndsAt.split(":")[1]), 0));
     const dateOk = new Date(Date.parse(dateFromCard)).getUTCDate() === new Date().getUTCDate();
@@ -12,7 +15,7 @@ export function defineHintsRequired(params: { lessonStartsAt: string; lessonEnds
     const isNow = dateOk && new Date() >= startDate && new Date() <= endDate;
 
     const timeFromNowToLessonStart = moment().to(startDate, true);
-    const isPending = dateOk && timeFromNowToLessonStart.includes("minutes") && Number.parseInt(timeFromNowToLessonStart) <= 30;
+    const isPending = dateOk && timeFromNowToLessonStart.includes("minutes") && Number.parseInt(timeFromNowToLessonStart) <= minutesBeforeStartLesson;
 
-    return { isNow: isNow, isPending };
+    return { isNow, isPending };
 }
