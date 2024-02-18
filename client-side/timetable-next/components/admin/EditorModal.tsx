ec16bc5a2e0f266b676adace9b6eed1cc9183aa1
Aleksandr <sanya.romaniuta@gmail.com>
2024-02-18 21:46:17 +0300

    добавил переиспользуемое модальное окно редактирования данных для админки

---
+++ client-side/timetable-next/components/admin/EditorModal.tsx
@@ -0,0 +1,29 @@
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, ResponsiveValue } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+type Props = {
+    isOpen: boolean;
+    onClose: () => void;
+    size: ResponsiveValue<string> | undefined;
+    children: ReactNode | ReactNode[];
+};
+
+export default function EditorModal({ isOpen, onClose, size, children }: Props) {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            size={size}
+        >
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Изменение данных</ModalHeader>
+                <ModalCloseButton />
+
+                <ModalBody>{children}</ModalBody>
+
+                <ModalFooter />
+            </ModalContent>
+        </Modal>
+    );
+}
