2a048131c2b66bc3d59a9ba24e9f395c1c0bb875
Aleksandr <sanya.romaniuta@gmail.com>
2024-03-03 23:02:46 +0300

    разбираюсь с условными типами в ветч враппере

--- client-side/timetable-next/server-actions/actionsFetchWrappers.ts
+++ client-side/timetable-next/server-actions/actionsFetchWrappers.ts
@@ -1,21 +1,18 @@
 import authOptions from "@/configs/authConfig";
 import { serviceResultSchema } from "@/fetching/admin/zodSchemas";
-import ServiceResult from "@/types/serviceResult";
 import { getServerSession } from "next-auth";
 import { SafeParseReturnType } from "zod";
 
-export interface ClientResult {
-    success: boolean | null;
-    message: string;
-}
-
+type FetchWrapperResult<T> = T extends undefined | never | null
+    ? { success: boolean; description: string }
+    : { success: boolean; description: string; value?: T };
 type PutParams<T> = { url: string; entity: T; revalidateFn: () => void };
-type delParams = { url: string; id: number; revalidateFn: () => void };
-type CheckIsAuthorizedParams = { success: false, description: string } |  { success: true, token: string }
+type DelParams = { url: string; id: number; revalidateFn: () => void };
+type HandleOkStatusCodeParams<T> = { responseParsed: SafeParseReturnType<FetchWrapperResult<T>, FetchWrapperResult<T>>; revalidateFn: () => void };
+type CheckIsAuthorizedParams = { success: false; description: string } | { success: true; token: string };
 
-const msg = "Что-то пошло не так, смотрите логи на сервере.";
-
-    const auth =  await checkIsAuthorized();
+export async function putEntity<T>({ url, entity, revalidateFn }: PutParams<T>): Promise<FetchWrapperResult<T | null>> {
+    const auth = await checkIsAuthorized();
     if (!auth.success) {
         return auth;
     }
@@ -30,9 +27,7 @@ const msg = "Что-то пошло не так, смотрите логи на 
             body: JSON.stringify(entity),
         });
 
-        if (!response.ok) {
-            return await handleBadStatusCode(response);
-        }
+        if (!response.ok) return await handleBadStatusCode(response);
 
         const responseParsed = serviceResultSchema.safeParse(await response.json());
         return handleOkStatusCode({ responseParsed: responseParsed, revalidateFn: revalidateFn });
@@ -40,7 +35,7 @@ const msg = "Что-то пошло не так, смотрите логи на 
         return handleFetchException(error);
     }
 }
-
+export async function deleteEntity({ url, id, revalidateFn }: DelParams): Promise<FetchWrapperResult<undefined>> {
     const auth = await checkIsAuthorized();
     if (!auth.success) {
         return auth;
@@ -54,9 +49,7 @@ const msg = "Что-то пошло не так, смотрите логи на 
             },
         });
 
-        if (!response.ok) {
-            return await handleBadStatusCode(response);
-        }
+        if (!response.ok) return await handleBadStatusCode(response);
 
         const responseParsed = serviceResultSchema.safeParse(await response.json());
         return handleOkStatusCode({ responseParsed: responseParsed, revalidateFn: revalidateFn });
@@ -65,69 +58,44 @@ const msg = "Что-то пошло не так, смотрите логи на 
     }
 }
 
-async function handleBadStatusCode(response: Response): Promise<ClientResult> {
-    switch (response.status) {
-        case 400:
-            const responseParsed = serviceResultSchema.safeParse(await response.json());
-
-            if (responseParsed.success) {
-                console.error(responseParsed.data);
-                return { message: responseParsed.data.description, success: false };
-            } else {
-                console.error(responseParsed.error.flatten());
-                return { message: msg, success: false };
-            }
-
-        case 401:
-            return { message: "Отсутствует аутентификация. 401", success: false };
-
-        case 403:
-            return { message: "Отсутствуют права. 403", success: false };
-
-        case 404:
-            return { message: "Endpoint не найден. 404", success: false };
-
-        case 502:
-            return { message: "Скорее всего Nginx Bad Gateway. 502", success: false };
-
-        case 500:
-            return { message: "Похоже, что посыпались исключения на бекенде. 500", success: false };
-
-        default:
-            return { message: `Что-то пошло не так. Код ошибки: ${response.status}:${response.statusText}`, success: false };
-    }
-}
-
-function handleOkStatusCode({
-    responseParsed,
-    revalidateFn,
-}: {
-    responseParsed: SafeParseReturnType<ServiceResult, ServiceResult>;
-    revalidateFn: () => void;
-}) {
+function handleOkStatusCode<T>({ responseParsed, revalidateFn }: HandleOkStatusCodeParams<T>): FetchWrapperResult<T | undefined> {
     const zodIsValid = responseParsed.success;
     const apiIsSuccess = zodIsValid ? responseParsed.data.success : false;
 
     if (zodIsValid && apiIsSuccess) {
         revalidateFn();
-        return { message: responseParsed.data.description, success: true };
+        return { description: responseParsed.data.description, success: true, value: responseParsed.data.value as T } satisfies FetchWrapperResult<T>;
     } else {
         if (!zodIsValid) console.error(responseParsed.error.flatten());
-        return { message: msg, success: false };
+        return { success: false, description: "Получен неожиданный ответ от API." };
     }
 }
 
-function handleFetchException(error: unknown) {
+async function handleBadStatusCode(response: Response): Promise<FetchWrapperResult<null>> {
+    try {
+        console.error({ statusCode: response.status, json: await response.json() });
+    } catch {
+        console.error({ statusCode: response.status, body: await response.text() });
+    } finally {
+        return { success: false, description: `Что-то пошло не так. Код ошибки: ${response.status}:${response.statusText}` };
+    }
+}
+
+function handleFetchException(error: unknown): FetchWrapperResult<undefined> {
     console.error(error);
+    return { success: false, description: "Произошла ошибка на сервере во время выполнения запроса." };
+}
+
 async function checkIsAuthorized(): Promise<CheckIsAuthorizedParams> {
     const session = await getServerSession(authOptions);
 
     if (!session || !session.accessToken) {
         return { success: false, description: "Сессия или токен доступа не получены." };
     }
+
     if (!session.roles?.includes("admin")) {
         return { success: false, description: "Вы не являетесь админом." };
-}
+    }
 
     return { success: true, token: session.accessToken };
 }
