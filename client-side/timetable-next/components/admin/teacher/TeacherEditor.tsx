fdf8b23ca241fdd164f685ad2c00a98fe8b90b7d
Aleksandr <sanya.romaniuta@gmail.com>
2024-03-08 08:35:13 +0300

    закончил страничку с перподами

--- client-side/timetable-next/components/admin/teacher/TeacherEditor.tsx
+++ client-side/timetable-next/components/admin/teacher/TeacherEditor.tsx
@@ -1,8 +1,108 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { getTeachersSchema } from "@/fetching/admin/zodSchemas";
 import { z } from "zod";
+import { Button, HStack, Td, Th, Tr, VStack, useDisclosure } from "@chakra-ui/react";
+import { useImmer } from "use-immer";
+import genericSort from "@/utils/genericSort";
+import EditorTable from "../EditorTable";
+import SearchBar from "../SearchBar";
+import TeacherModal from "./TeacherModal";
 
 type TeacherType = z.infer<typeof getTeachersSchema.shape.data.shape.teachers.element>;
+type SortingType = {
+    searchQuery: string;
+    isAsc: boolean;
+    sortingField: keyof TeacherType;
+};
+const hover = { cursor: "pointer", color: "purple.300" };
 export default function TeacherEditor({ teachers }: { teachers: TeacherType[] }) {
-    return <div>TeacherEditor</div>;
+    const [sorting, setSorting] = useImmer<SortingType>({ isAsc: true, searchQuery: "", sortingField: "id" });
+    const initialTeacher: TeacherType = { id: 0, firstname: "", lastname: "", middlename: "", ascId: "", modifiedAt: new Date() };
+    const [selectedTeacher, setSelectedTeacher] = useState<TeacherType>(initialTeacher);
+    const disclosure = useDisclosure();
+
+    const localTeachers = teachers
+        .filter(t =>
+            ` ${t.ascId}  + ${t.id} + ${t.lastname} + ${t.firstname} + ${t.middlename}`.toUpperCase().includes(sorting.searchQuery.toUpperCase())
+        )
+        .sort((a, b) => genericSort(sorting.sortingField, sorting.isAsc, a, b));
+
+    const tableHeaders = (
+        <>
+            <Th
+                _hover={hover}
+                onClick={() =>
+                    setSorting(draft => {
+                        draft.sortingField = "id";
+                        draft.isAsc = !draft.isAsc;
+                    })
+                }
+            >
+                Id
+            </Th>
+
+            <Th
+                _hover={hover}
+                onClick={() =>
+                    setSorting(draft => {
+                        draft.sortingField = "lastname";
+                        draft.isAsc = !draft.isAsc;
+                    })
+                }
+            >
+                ФИО
+            </Th>
+
+            <Th _hover={hover}>Последнее изменение</Th>
+        </>
+    );
+
+    const tableBody = localTeachers.map(s => (
+        <Tr
+            key={s.id}
+            _hover={{ cursor: "pointer", color: "purple.300" }}
+            onClick={() => {
+                setSelectedTeacher(s);
+                disclosure.onOpen();
+            }}
+        >
+            <Td>{s.id}</Td>
+            <Td>{`${s.lastname} ${s.firstname} ${s.middlename}`}</Td>
+            <Td>{s.modifiedAt.toLocaleString("ru-ru")}</Td>
+        </Tr>
+    ));
+
+    return (
+        <>
+            <VStack
+                borderWidth={"1px"}
+                borderRadius={"md"}
+                maxW={"8xl"}
+                mx={"auto"}
+                mt={20}
+            >
+                <HStack mt={3}>
+                    <SearchBar onChange={e => setSorting(draft => void (draft.searchQuery = e.target.value))} />
+                    <Button
+                        colorScheme="green"
+                        onClick={() => {
+                            setSelectedTeacher(initialTeacher);
+                            disclosure.onOpen();
+                        }}
+                    >
+                        Добавить
+                    </Button>
+                </HStack>
+
+                <EditorTable tableHeaders={tableHeaders}> {tableBody}</EditorTable>
+            </VStack>
+
+            <TeacherModal
+                disclosure={disclosure}
+                key={selectedTeacher.id}
+                selectedTeacher={selectedTeacher}
+            ></TeacherModal>
+        </>
+    );
 }
--- client-side/timetable-next/components/admin/teacher/TeacherModal.tsx
+++ client-side/timetable-next/components/admin/teacher/TeacherModal.tsx
@@ -1,5 +1,94 @@
+import { Button, HStack, Input, VStack, useToast, Text, UseDisclosureReturn } from "@chakra-ui/react";
 import React from "react";
+import { z } from "zod";
+import EditorModal from "../EditorModal";
+import ReadonlyEditorInputs from "../ReadonlyEditorInputs";
+import { getTeachersSchema } from "@/fetching/admin/zodSchemas";
+import { deleteTeacher, putTeacher } from "@/server-actions/teacherActions";
 
-export default function TeacherModal() {
-    return <div>TeacherModal</div>;
+type TeacherType = z.infer<typeof getTeachersSchema.shape.data.shape.teachers.element>;
+export default function TeacherModal({ disclosure, selectedTeacher }: { disclosure: UseDisclosureReturn; selectedTeacher: TeacherType }) {
+    const toast = useToast({ duration: 5000, isClosable: true });
+    const successfulToast = (message: string) => toast({ status: "success", title: "Данные сохранены", description: message });
+    const failedToast = (message: string) => toast({ status: "error", title: "Не удалось выполнить операцию", description: message });
+    const loadingToast = (title: string) => toast({ status: "loading", title: title });
+
+    return (
+        <EditorModal
+            {...disclosure}
+            size={"xl"}
+        >
+            <form
+                onSubmit={async e => {
+                    e.preventDefault();
+                    const toastId = loadingToast("Сохранение данных...");
+                    const result = await putTeacher(new FormData(e.currentTarget));
+                    result.success ? successfulToast(result.message) : failedToast(result.message);
+                    toast.close(toastId);
+                }}
+            >
+                <VStack>
+                    <ReadonlyEditorInputs
+                        id={selectedTeacher.id}
+                        modifiedAt={selectedTeacher.modifiedAt}
+                    />
+
+                    <HStack w={"full"}>
+                        <Text>Фамилия</Text>
+                        <Input
+                            defaultValue={selectedTeacher.lastname}
+                            name="lastname"
+                        ></Input>
+                    </HStack>
+
+                    <HStack w={"full"}>
+                        <Text>Имя</Text>
+                        <Input
+                            defaultValue={selectedTeacher.firstname}
+                            name="firstname"
+                        ></Input>
+                    </HStack>
+
+                    <HStack w={"full"}>
+                        <Text>Отчество</Text>
+                        <Input
+                            defaultValue={selectedTeacher.middlename}
+                            name="middlename"
+                        ></Input>
+                    </HStack>
+
+                    <HStack w={"full"}>
+                        <Text>AscID</Text>
+                        <Input
+                            defaultValue={selectedTeacher.ascId!}
+                            name="ascId"
+                        ></Input>
+                    </HStack>
+
+                    <HStack
+                        mt={2}
+                        w={"full"}
+                        justifyContent={"space-around"}
+                    >
+                        <Button
+                            colorScheme="red"
+                            onClick={async () => {
+                                const res = await deleteTeacher({ id: selectedTeacher.id });
+                                res.success ? successfulToast(res.message) : failedToast(res.message);
+                            }}
+                        >
+                            Удалить
+                        </Button>
+
+                        <Button
+                            type="submit"
+                            colorScheme="blue"
+                        >
+                            Сохранить
+                        </Button>
+                    </HStack>
+                </VStack>
+            </form>
+        </EditorModal>
+    );
 }
