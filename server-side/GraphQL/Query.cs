0f92033820bd27fb9c3651d5e6e45c70efd6df5b
Aleksandr <sanya.romaniuta@gmail.com>
2024-03-12 09:09:35 +0300

    поправил форматирование

--- server-side/GraphQL/Query.cs
+++ server-side/GraphQL/Query.cs
@@ -8,15 +8,15 @@ namespace GraphQL
 {
     public class Query
     {
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<Subject> GetSubjects([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Subjects;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<Teacher> GetTeachers([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Teachers;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<Group> GetGroups([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Groups;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<Room> GetRooms([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Cabinets;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<LessonTime> GetLessonTimes([Service(ServiceKind.Synchronized)] TimetableContext context) => context.LessonTimes;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<ActualCard> GetActualCards([Service(ServiceKind.Synchronized)] TimetableContext context) => context.ActualCards;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<ActualTimetable> GetActualTimetables([Service(ServiceKind.Synchronized)] TimetableContext context) => context.ActualTimetables;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<StableCard> GetStableCards([Service(ServiceKind.Synchronized)] TimetableContext context) => context.StableCards;
-        [UseProjection][UseFiltering][UseSorting] public IQueryable<StableTimetable> GetStableTimetables([Service(ServiceKind.Synchronized)] TimetableContext context) => context.StableTimetables;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<Subject> GetSubjects([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Subjects;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<Teacher> GetTeachers([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Teachers;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<Group> GetGroups([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Groups;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<Room> GetRooms([Service(ServiceKind.Synchronized)] TimetableContext context) => context.Cabinets;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<LessonTime> GetLessonTimes([Service(ServiceKind.Synchronized)] TimetableContext context) => context.LessonTimes;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<ActualCard> GetActualCards([Service(ServiceKind.Synchronized)] TimetableContext context) => context.ActualCards;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<ActualTimetable> GetActualTimetables([Service(ServiceKind.Synchronized)] TimetableContext context) => context.ActualTimetables;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<StableCard> GetStableCards([Service(ServiceKind.Synchronized)] TimetableContext context) => context.StableCards;
+        [UseProjection, UseFiltering, UseSorting] public IQueryable<StableTimetable> GetStableTimetables([Service(ServiceKind.Synchronized)] TimetableContext context) => context.StableTimetables;
         public IEnumerable<WeekPeriod> GetWeekNumbers([Service(ServiceKind.Synchronized)] TimetableContext context, int groupId)
         {
             var weekNumbers = context.ActualTimetables.Where(at=>at.GroupId == groupId).Select(at => at.WeekNumber).Distinct().ToList();
