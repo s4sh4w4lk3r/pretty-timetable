11c47973b53d611a0aced109f5ba70ab1783f80d
Aleksandr <sanya.romaniuta@gmail.com>
2024-03-13 17:09:24 +0300

    небольшие изменения в weekSelector

--- client-side/timetable-next/components/admin/timetables/WeekSelector.tsx
+++ client-side/timetable-next/components/admin/timetables/WeekSelector.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { getWeekNumbersSchema } from "@/fetching/admin/zodSchemas";
 import { Select } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { z } from "zod";
 
 type WeekType = z.infer<typeof getWeekNumbersSchema.shape.data.shape.weekNumbers.element>;
 export default function WeekSelector({ weekNumbers }: { weekNumbers: WeekType[] }) {
+    const groupId = usePathname().split("/").at(3);
     const router = useRouter();
+
     const weekNumberOptions = weekNumbers.map(w => (
         <option
             key={w.weekNumber}
@@ -19,7 +21,7 @@ export default function WeekSelector({ weekNumbers }: { weekNumbers: WeekType[] 
 
     return (
         <Select
-            onChange={e => (e.target.value ? router.push(`actual?weeknumber=${e.target.value}`) : null)}
+            onChange={e => (e.target.value ? router.push(`/admin/timetables/${groupId}/actual?weeknumber=${e.target.value}`) : null)}
             placeholder="Выберите неделю"
             name="weekSelector"
         >
