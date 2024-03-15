e7b22dd7f653f8e0d1a307c6e456fd4c3c4de748
Aleksandr <sanya.romaniuta@gmail.com>
2024-03-15 10:12:39 +0300

    вынес модальное выбора групп в отдельный компонент

--- client-side/timetable-next/components/admin/group/GroupEditor.tsx
+++ client-side/timetable-next/components/admin/group/GroupEditor.tsx
@@ -1,64 +1,18 @@
 "use client";
-import { Button, HStack, Input, Td, Text, Th, Tr, VStack, useDisclosure } from "@chakra-ui/react";
+import { Button, HStack, Text, useDisclosure } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { z } from "zod";
-import EditorModal from "../EditorModal";
-import { AddIcon, CloseIcon } from "@chakra-ui/icons";
-import EditorTable from "../EditorTable";
-import { usePathname, useRouter } from "next/navigation";
-import { deleteGroup, putGroup } from "@/server-actions/groupActions";
-import useToasts from "@/utils/client/useToasts";
+import { usePathname } from "next/navigation";
 import { getAllGroupsSchema } from "@/fetching/zodSchemas";
+import GroupModal from "./GroupModal";
 
 type GroupType = z.infer<typeof getAllGroupsSchema.shape.data.shape.groups.element>;
 export default function GroupEdtior({ groups }: { groups: GroupType[] }) {
-    const [query, setQuery] = useState("");
     const disclosure = useDisclosure();
-    const router = useRouter();
+
     const groupIdFromPath: number = Number.parseInt(usePathname().split("/")[3]);
     const groupFromPath = groupIdFromPath ? groups.find(g => g.id === groupIdFromPath) : undefined;
-    const [group, setGroup] = useState<GroupType>(groupFromPath ? groupFromPath : { id: 0, name: "", modifiedAt: new Date() });
-    const { toast, successfulToast, failedToast, loadingToast } = useToasts();
-
-    const localGroups = groups.filter(g => g.name.toLowerCase().includes(query.toLowerCase())).sort();
-
-    const tableBody = localGroups.map(g => (
-        <Tr
-            key={g.id}
-            _hover={{ cursor: "pointer", color: "purple.300" }}
-            onClick={() => {
-                setGroup(g);
-                disclosure.onClose();
-                router.push(`/admin/timetables/${g.id}/actual`);
-            }}
-        >
-            <Td>{g.id}</Td>
-            <Td>
-                <HStack
-                    w={"full"}
-                    justifyContent={"space-between"}
-                >
-                    <Text>{g.name}</Text>
-                    <Button
-                        onClick={async e => {
-                            e.stopPropagation();
-                            const toastId = loadingToast("Сохранение...");
-                            const res = await deleteGroup({ id: g.id });
-                            res.success ? successfulToast(res.message) : failedToast(res.message);
-                            toast.close(toastId);
-                        }}
-                        variant={"ghost"}
-                        size={"xs"}
-                    >
-                        <CloseIcon
-                            color="red.300"
-                            boxSize={3}
-                        />
-                    </Button>
-                </HStack>
-            </Td>
-        </Tr>
-    ));
+    const [selectedGroup, setSelectedGroup] = useState<GroupType>(groupFromPath ? groupFromPath : { id: 0, name: "", modifiedAt: new Date() });
 
     return (
         <>
@@ -67,7 +21,7 @@ export default function GroupEdtior({ groups }: { groups: GroupType[] }) {
                 justifyContent={"center"}
                 mb={2}
             >
-                <Text fontSize={"xl"}>{group.name ? group.name : "Группа не выбрана"}</Text>
+                <Text fontSize={"xl"}>{selectedGroup.name ? selectedGroup.name : "Группа не выбрана"}</Text>
                 <Button
                     colorScheme="purple"
                     onClick={() => {
@@ -77,44 +31,11 @@ export default function GroupEdtior({ groups }: { groups: GroupType[] }) {
                     Изменить
                 </Button>
             </HStack>
-
-            <EditorModal
-                {...disclosure}
-                size={"xl"}
-            >
-                <HStack mb={2}>
-                    <Input
-                        placeholder="Поиск по группам или добавление"
-                        onChange={e => {
-                            setQuery(e.target.value);
-                        }}
-                    />
-
-                    <Button
-                        colorScheme="green"
-                        size={"md"}
-                        onClick={async () => {
-                            const toastId = loadingToast("Сохранение...");
-                            const res = await putGroup({ name: query });
-                            res.success ? successfulToast(res.message) : failedToast(res.message);
-                            toast.close(toastId);
-                        }}
-                    >
-                        <AddIcon />
-                    </Button>
-                </HStack>
-
-                <VStack>
-                    <EditorTable tableHeaders={tableHeaders}>{tableBody}</EditorTable>
-                </VStack>
-            </EditorModal>
+            <GroupModal
+                groups={groups}
+                disclosure={disclosure}
+                onGroupSelected={setSelectedGroup}
+            />
         </>
     );
 }
-
-const tableHeaders = (
-    <>
-        <Th>Id</Th>
-        <Th>Название</Th>
-    </>
-);
