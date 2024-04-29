ab2988f3e07c1ac1486257f9ff9d6cda33b861c5
Aleksandr <sanya.romaniuta@gmail.com>
2024-04-29 20:59:58 +0300

    Cards endpoint

--- server-side/PrettyTimetable.WebApi/Controllers/Actual/ActualCardController.cs
+++ server-side/PrettyTimetable.WebApi/Controllers/Actual/ActualCardController.cs
@@ -1,8 +1,11 @@
 ﻿using Mappers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models.Request;
 using PrettyTimetable.Abstractions.Actual;
+using PrettyTimetable.Repository.Database;
+using System.Globalization;
 
 namespace PrettyTimetable.WebApi.Controllers.Actual
 {
@@ -24,6 +27,23 @@ namespace PrettyTimetable.WebApi.Controllers.Actual
             var result = await actualCardService.DeleteAsync(id);
 
             return result.Success is true ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet, Route("kachok/{groupName}")]
+        public IActionResult GetCards([FromRoute] string groupName, [FromQuery] bool isTomorrow, [FromServices] TimetableContext context)
+        {
+            DateTime dateTime = isTomorrow ? DateTime.Now.AddDays(1) : DateTime.Now;
+            int weekNumber = ISOWeek.GetWeekOfYear(dateTime);
+            DateOnly date = DateOnly.FromDateTime(dateTime);
+            var cards = context.ActualCards.Where(x => x.Date == date && x.RelatedTimetable!.WeekNumber == weekNumber && x.RelatedTimetable!.Group!.Name == groupName).OrderBy(x => x.LessonTime!.Number)
+                .Include(x=>x.Cabinet).Include(x=>x.Subject).Include(x=>x.Teacher).Include(x=>x.LessonTime).ToList();
+
+            if (cards.Count != 0)
+            {
+                return Ok(cards);
+            }
+
+            else return BadRequest();
         }
     }
 }
